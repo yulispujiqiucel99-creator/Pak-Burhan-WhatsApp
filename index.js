76aa567eb898455d4a8c56a0182dfaf9b1084f0d
yulispujiqiucel99-creator <yulispuji.qiucel99@gmail.com>
2026-09-15 21:59:46 +0700

    Restore index.js sementara (placeholder sebelumnya tidak sengaja)

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-PLACEHOLDER_TOO_LARGE
+/**
+ * Pak Burhan WhatsApp Bot
+ * Baileys + QR / Pairing Code + Google Gemini
+ *
+ * TEMPORARY RESTORE - full update coming
+ */
+console.log('Bot starting - full code restore in progress');
